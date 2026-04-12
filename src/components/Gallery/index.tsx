a488f2bea3d62cd8046e275a06609d4c23f207b3
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2026-04-13 01:54:20 +0900

    feat: add overlay component

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -5,44 +5,45 @@ import galeriaTenisRoxo from "@/assets/images/galeria-tenis-roxo.jpg";
 import galeriaTenisColorido from "@/assets/images/galeria-tenis-colorido.jpg";
 import galeriaModelo from "@/assets/images/galeria-modelo.jpg";
 import { Button } from "../Button";
+import { Overlay } from "../Overlay";
 
 
 export const Gallery = () => {
     return (
-        <section className="container grid grid-cols-1 md:grid-cols-2 gap-7.5">
-            <div className="grid grid-rows-[2fr_1fr] gap-7.5 h-full overflow-hidden ">
+        <section className="container max-h-227.5 grid grid-cols-1 gap-2.5 md:grid-cols-2 md:gap-7 ">
+            <div className="grid grid-rows-[2fr_1fr]  gap-2.5 md:gap-7  max-h-207.5 overflow-hidden ">
                 <div className="relative">
-                    <div className="rounded-2xl h-full w-full overflow-hidden">
-                        <img className="w-full h-full object-cover" src={cardImagem} alt="Imagem card" /></div>
-                    <div className="absolute inset-0 p-6 flex flex-col justify-center text-white text-center">
-                        <p className="font-semibold text-lg">Kripton One</p>
-                        <h2 className="text-3xl font-bold mb-4">Estilo urbando com atitude</h2>
-                        <div className="flex justify-center items-center gap-4">
-                            <Button variant="secondary">Feminino</Button>
-                            <Button variant="secondary">Masculino</Button>
-                        </div>
+                    <div className="rounded-2xl overflow-hidden">
+                        <img className="w-full h-full object-cover" src={cardImagem} alt="Imagem card" />
                     </div>
+
+                    <Overlay
+                        title="Kripton One"
+                        subtitle="Estilo urbando com atitude"
+                        className="inset-0 p-6 flex flex-col justify-center text-white text-center">
+                        <Button variant="secondary">Feminino</Button>
+                        <Button variant="secondary">Masculino</Button>
+                    </Overlay>
                 </div>
-                <div className="rounded-2xl h-full w-full overflow-hidden">
-                    <img className="w-full h-full" src={galeriaTenisBrancoPreto} alt="Tenis preto e branco" />
+                <div className="rounded-2xl h-full w-full overflow-hidden ">
+                    <img className="w-full h-full object-cover" src={galeriaTenisBrancoPreto} alt="Tenis preto e branco" />
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-7.5 ">
+            <div className="grid grid-cols-2 gap-2.5 max-h-207.5 md:gap-7 ">
                 <div className="col-span-2 rounded-2xl overflow-hidden order-last md:order-first">
-                    <img className="w-full h-full " src={galeriaTenisRoxo} alt="Tenis roxo" />
+                    <img className="w-full h-full object-cover " src={galeriaTenisRoxo} alt="Tenis roxo" />
                 </div>
-                <div className="row-span-2 rounded-2xl overflow-hidden h-full">
-                    <img className="w-full h-full " src={galeriaModelo} alt="Modelo" />
+                <div className="row-span-2 overflow-hidden rounded-2xl  ">
+                    <img className="w-full h-full object-cover  " src={galeriaModelo} alt="Modelo" />
                 </div>
-
-                <div className="rounded-2xl h-full w-full overflow-hidden">
-                    <img className="w-full h-full " src={galeriaTenisCinza} alt="Tenis Cinza" />
-                </div>
-                <div className="rounded-2xl h-full w-full overflow-hidden">
-                    <img className="h-full w-full " src={galeriaTenisColorido} alt="Tenis colorido" />
-                </div>
+               
+                    <div className="rounded-2xl overflow-hidden flex justify-center items-center ">
+                        <img className="h-full w-full object-cover " src={galeriaTenisColorido} alt="Tenis colorido" />
+                    </div>
+                    <div className="rounded-2xl h-full w-full overflow-hidden">
+                        <img className="w-full h-full object-cover " src={galeriaTenisCinza} alt="Tenis Cinza" />
+                    </div>
             </div>
-
         </section>
     );
 };
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,24 +1,18 @@
 import Banner from "@/assets/images/banner.jpg";
+import { Overlay } from "../Overlay";
 import { Button } from "../Button";
+
+
 
 export const Hero = () => {
     return (
         <div className="container ">
             <section className="relative h-125 rounded-[20px] mb-10">
                 <img src={Banner} alt="Banner principal SyntaxWear" className="w-full h-full object-cover rounded-[20px]" />
-
-                <div className="absolute w-full bottom-0 flex justify-end items-center text-center px-6 md:px-24 pb-32 ">
-                    <div className="flex flex-col items-center text-white w-97">
-                        <h2 className="text-xl font-medium leading-normal tracking-wider mb-2.5">Kryton One</h2>
-                        <h1 className="text-2xl leading-9 tracking-widest mb-10">Transforme qualquer passo em presença</h1>
-
-                        <div className="flex gap-3.5 ">
-                            <Button variant="secondary" size="sm">Ver modelos</Button>
-                            <Button variant="primary" >Comprar</Button>
-                        </div>
-                    </div>
-
-                </div>
+                 <Overlay title="Kriton One" subtitle="Transforme qualquer passo em presença" className="bottom-0 justify-end px-6 md:px-24 pb-32 md:items-end">
+                    <Button variant="secondary">ver modelos</Button>
+                    <Button>Comprar</Button>
+            </Overlay>
             </section>
         </div>
     );
